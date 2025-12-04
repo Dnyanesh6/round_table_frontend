cc7f9b7556f4ca0c2d540c27690eb337e671c95a
Dnyanesh U. Chaudhari <dnyaneshchaudhari05@gmail.com>
2025-12-04 10:21:46 +0530

    Refactor SignIn and SignUp components to remove Google sign-in functionality and simplify form layout

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -5,40 +5,16 @@ import React, { useEffect } from "react";
     export default function SignIn() {
     // const [session, setSession] = React.useState<any>(null);
 
-    // sign in function (google)
-    const handleSignIn = () => {
-        window.location.href = `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/auth/google`;
-    }
-
         return (
             <div className='bg-white h-screen w-screen text-black flex justify-center items-center px-4'>
                 <div className='flex flex-col justify-center items-center  border-2 border-gray-300 w-full sm:w-[400px] md:w-[500px] lg:w-[600px] rounded-lg gap-6'>
-                    <div className='text-2xl sm:text-3xl md:text-4xl text-center font-bold mb-8 text-left'>
+                    <div className='text-2xl pt-2 sm:text-3xl md:text-4xl text-center font-bold mb-8 text-left'>
                         Welcome Back
                     </div>
 
-
-                    {/* social sign up options */}
-                    <div className='flex flex-col justify-center items-center gap-2'>
-                        <div
-                        onClick={handleSignIn}
-                        className=' p-2 border border-2 w-full border-gray-300 rounded-md'>
-                            Sign In with Google
-                        </div>
-
-                        <div className=' p-2 border border-2 border-gray-300 w-full rounded-md'>
-                            Sign In with Apple
-                        </div>
-
-                        <div className='p-2 border border-2 border-gray-300 w-full rounded-md'>
-                            Sign In with Twitter
-                        </div>
-
-                        <div className='flex flex-row justify-center items-center'>Or</div>
-
                         {/* // Email Sign Up Form */}
                         <div>
-                            <form className='flex flex-col gap-4'>
+                            <form className='flex flex-col gap-4 py-4 pb-6'>
                                 <input type="email" placeholder='Email' className='text-xl sm:text-3xl md:text-4xl text-center p-2 border border-2 border-gray-300 rounded-md' />
                                 <input type="password" placeholder='Password' className='text-xl sm:text-3xl md:text-4xl text-center p-2 border border-2 border-gray-300 rounded-md' />
                                 <button type="submit" className='p-2 border border-2 border-blue-500 bg-blue-200 rounded-full'>Continue</button>
@@ -46,6 +22,5 @@ import React, { useEffect } from "react";
                         </div>
                     </div>
                 </div>
-            </div>
         )
     }
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -5,41 +5,16 @@ export default function SignUp(){
     
     // const router = useRouter();
 
-const handleSignUp = () => {
-    try {
-        window.location.href = process.env.NEXT_PUBLIC_BACKEND_URL + '/api/v1/auth/google';
-    } catch (error) {
-        console.log('Error during sign-up:', error);
-    }
-}
-
     return(
         <div className='bg-white h-screen w-screen text-black flex justify-center items-center px-4'>
             <div className='flex flex-col justify-center items-center  border-2 border-gray-300 p-6 w-full sm:w-[400px] md:w-[500px] lg:w-[600px] rounded-lg gap-6'>
-                <div className='text-2xl sm:text-3xl md:text-4xl text-center font-bold mb-8 text-left'>
+                <div className='text-2xl pt-2 sm:text-3xl md:text-4xl text-center font-bold mb-8 text-left'>
                     Welcome
                 </div>
 
-                {/* social sign in options */}
-                <div className='flex flex-col justify-center items-center gap-2'>
-                    <button 
-                    onClick={handleSignUp}
-                    className=' p-2 border border-2 border-gray-300 w-full rounded-md'>
-                        Sign in with Google
-                    </button>
-                    <div className=' p-2 border border-2 border-gray-300 w-full rounded-md'>
-                        Sign in with Apple
-                    </div>
-
-                    <div className='p-2 border border-2 border-gray-300 w-full rounded-md'>
-                        Sign in with Twitter
-                    </div>
-
-                    <div className='flex flex-row justify-center items-center'>Or</div>
-
                     {/* // Email Sign In Form */}
                     <div>
-                        <form className='flex flex-col gap-4'>
+                        <form className='flex flex-col py-4  gap-4'>
                             <input type="email" placeholder='Email' className='text-2xl sm:text-3xl md:text-4xl text-center p-2 border border-2 border-gray-300 rounded-md' />
                             <input type="password" placeholder='Password' className='text-2xl sm:text-3xl md:text-4xl text-center p-2 border border-2 border-gray-300 rounded-md' />
                             <input type="password" placeholder='Confirm Password' className='text-2xl sm:text-3xl md:text-4xl text-center p-2 border border-2 border-gray-300 rounded-md' />
@@ -50,6 +25,5 @@ const handleSignUp = () => {
                     </div>
                 </div>
             </div>
-        </div>
     )
 }
